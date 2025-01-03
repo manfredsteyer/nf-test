990037be5561a51e8261974a7656987bff72602d
Manfred Steyer <manfred.steyer@gmx.net>
2025-01-03 20:04:24 +0100

    add kendo-ui demo

--- projects/kendo-ui-app/src/app/app.component.ts
+++ projects/kendo-ui-app/src/app/app.component.ts
@@ -1,12 +1,54 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { GridDataResult, PageChangeEvent, GridModule } from '@progress/kendo-angular-grid';
+import { SortDescriptor } from '@progress/kendo-data-query';
+// import { FilterDescriptor } from "@progress/kendo-data-query";
+import { ProductService } from './product.service';
+import { Observable } from 'rxjs';
+import { categories, Category } from './data.products';
+import { DropDownsModule } from '@progress/kendo-angular-dropdowns';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    imports: [GridModule, DropDownsModule, CommonModule],
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
+    providers: [ProductService]
 })
 export class AppComponent {
-  title = 'kendo-ui-app';
+    // used for the DropDownList
+    public dropDownItems: Category[] = categories;
+    public defaultItem: Category = { text: 'Filter by Category', value: null };
+
+    // used for the Grid
+    public gridItems: Observable<GridDataResult> | undefined;
+    public pageSize: number = 10;
+    public skip: number = 0;
+    public sortDescriptor: SortDescriptor[] = [];
+    // public filterDescriptor: FilterDescriptor[] = [];
+    public filterTerm: number | null = null;
+
+    constructor(private service: ProductService) {
+        this.loadGridItems();
+    }
+
+    public pageChange(event: PageChangeEvent): void {
+        this.skip = event.skip;
+        this.loadGridItems();
+    }
+
+    private loadGridItems(): void {
+        this.gridItems = this.service.getProducts(this.skip, this.pageSize, this.sortDescriptor, this.filterTerm);
+    }
+
+    public handleSortChange(descriptor: SortDescriptor[]): void {
+        this.sortDescriptor = descriptor;
+        this.loadGridItems();
+    }
+
+    public handleFilterChange(item: Category): void {
+        this.filterTerm = item.value;
+        this.skip = 0;
+        this.loadGridItems();
+    }
 }
