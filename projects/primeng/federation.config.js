e444d87f1c685b15600cb9b069d46635e41d2ced
Manfred Steyer <manfred.steyer@gmx.net>
2025-01-16 20:10:34 +0100

    add build flag to primeng theme

--- projects/primeng/federation.config.js
+++ projects/primeng/federation.config.js
@@ -1,30 +1,49 @@
-const { withNativeFederation, shareAll } = require('@angular-architects/native-federation/config');
-const globalSkipList = require('../../global-skip-list');
+const {
+  withNativeFederation,
+  shareAll,
+  share
+} = require("@angular-architects/native-federation/config");
+const globalSkipList = require("../../global-skip-list");
 
 module.exports = withNativeFederation({
-
-  name: 'primeng',
+  name: "primeng",
 
   exposes: {
-    './Component': './projects/primeng/src/app/app.component.ts',
+    "./Component": "./projects/primeng/src/app/app.component.ts",
   },
 
   shared: {
-    ...shareAll({ singleton: true, strictVersion: true, requiredVersion: 'auto', transient: true }),
+    ...shareAll({
+      singleton: true,
+      strictVersion: true,
+      requiredVersion: "auto",
+      transient: true,
+    }),
+    ...share({
+      "@primeng/themes/aura": {
+        singleton: true,
+        strictVersion: true,
+        requiredVersion: "auto",
+        transient: true,
+        includeSecondaries: false,
+        build: 'separate'
+      },
+    }),
   },
 
   skip: [
-    'rxjs/ajax',
-    'rxjs/fetch',
-    'rxjs/testing',
-    'rxjs/webSocket',
+    "rxjs/ajax",
+    "rxjs/fetch",
+    "rxjs/testing",
+    "rxjs/webSocket",
 
-    ...globalSkipList
+    ...globalSkipList,
 
     // Add further packages you don't need at runtime
-  ]
+  ],
 
   // Please read our FAQ about sharing libs:
   // https://shorturl.at/jmzH0
-  
 });
+
+// console.log("module.exports", module.exports);
